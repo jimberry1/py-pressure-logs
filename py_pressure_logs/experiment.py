a2e611c4aa99b505212c9e31b0c5e2e0b20a88b1
jim.berry <jim.berry@signal-ai.com>
2024-11-10 18:05:18 +0000

    clean up abstraction of experiment

--- py_pressure_logs/experiment.py
+++ py_pressure_logs/experiment.py
@@ -1,7 +1,9 @@
 from abc import ABC, abstractmethod
 from collections import defaultdict
+from itertools import dropwhile, takewhile
 import py_pressure_logs.regex_utils as regex_utils
 from typing import Dict, List
+import csv
 
 
 class Experiment(ABC):
@@ -13,9 +15,9 @@ class Experiment(ABC):
     experiments: Dict[str, Dict]
     summary: Dict[str, Dict]
 
-    def __init__(self, logs):
-        self.logs = logs
-        self.experiments = self.group_into_experiments(logs)
+    def __init__(self, file_path):
+        self.logs = self.parse_csv(file_path)
+        self.experiments = self.extract_experiments(self.logs)
 
     def summarise_experiment(self):
         """Given the logs for an experiment, calculates the maximum pressure values for each pump"""
@@ -34,60 +36,96 @@ class Experiment(ABC):
         self.summary = experiment_summary
         return experiment_summary
 
-    def is_time_field(header):
+    def is_time_field(self, header):
         pattern = r"time"
         return regex_utils.regex_match(pattern, header, ignore_case=True)
 
-    def is_output_field(header):
+    def is_output_field(self, header):
         pattern = r"output"
         return regex_utils.regex_match(pattern, header, ignore_case=True)
 
-    def group_into_experiments(self, rows):
-        row_iterator = iter(rows)
-        grouped_results = {}
+    def select_common_keys(self, raw_csv_row):
+        """Selects common keys shared between all experiments.
+        Returns a map of data to be combined with experiment specific keys."""
+        formatted_row = {}
+        for key, val in raw_csv_row.items():
+            if self.is_time_field(key):
+                formatted_row[self.TIME_KEY] = val
+                continue
+            elif self.is_output_field(key):
+                formatted_row[self.OUTPUT_KEY] = val
+                continue
+        return formatted_row
+
+    def parse_csv(self, file_path):
+        """
+        Parses a CSV file, renames headers, and selects specific keys.
+
+        :param file_path: Path to the CSV file.
+        :return: List of dictionaries with renamed and filtered keys.
+        """
+        parsed_data = []
+
+        with open(file_path, mode="r", newline="", encoding="utf-8") as csvfile:
+            reader = csv.DictReader(csvfile)
+
+            parsed_data = [
+                {
+                    **self.select_common_keys(row),
+                    **self.select_experiment_keys(row),
+                    "row_number": row_number,
+                }
+                for row_number, row in enumerate(reader, start=2)
+            ]
+
+        return parsed_data
+
+    def extract_experiments(self, logs):
+        """Iterates through the logs for the experimental run, extracting those relevant to experiments
+        and grouping them in an associative structure."""
+        log_iterator = iter(logs)
+        grouped_experiment_logs = {}
         try:
             while True:
-                experiment_rows = self.extract_experiment_logs(row_iterator)
-                print(f"found these logs {len(experiment_rows)}")
-                if not experiment_rows or len(experiment_rows) == 0:
+                logs_from_exp_start = dropwhile(
+                    lambda row: not self.is_start_of_experiment(row), log_iterator
+                )
+                experiment_logs = list(
+                    takewhile(
+                        lambda row: not self.is_end_of_experiment(row),
+                        logs_from_exp_start,
+                    )
+                )
+
+                if not experiment_logs or len(experiment_logs) == 0:
                     break
 
                 experiment_number = self.get_experiment_number_from_start_row(
-                    experiment_rows[0]
+                    experiment_logs[0]
                 )
-                grouped_results[experiment_number] = experiment_rows
+                grouped_experiment_logs[experiment_number] = experiment_logs
 
         except StopIteration:
             print("The experimental logs have been exhausted")
 
-        return grouped_results
+        return grouped_experiment_logs
 
     @abstractmethod
-    def is_pressure_field(self, row):
-        """Experiment specific regex to identify pressure field"""
-        pass
-
-    @abstractmethod
-    def transform_row(self, row):
+    def select_experiment_keys(self, row):
         """Experiment specific transformation function to extract important values"""
         pass
 
     @abstractmethod
     def get_experiment_number_from_start_row(self, start_row):
-        """Experiment specific regex match function to identify experiment number from first row"""
+        """Experiment specific regex match function to identify experiment number from first row of experiment logs"""
         pass
 
     @abstractmethod
     def is_start_of_experiment(self, row):
-        """Experiment specific regex match function to identify start of experiment"""
+        """Experiment specific regex match function to identify whether a row marks the start of an experiment"""
         pass
 
     @abstractmethod
     def is_end_of_experiment(self, row):
-        """Experiment specific regex match function to identify end of experiment"""
-        pass
-
-    @abstractmethod
-    def extract_experiment_logs(self, row_iterator):
-        """Experiment specific function to group log lines into experiments"""
+        """Experiment specific regex match function to identify whether a row marks the end of an experiment"""
         pass
--- py_pressure_logs/sunshine_experiment.py
+++ py_pressure_logs/sunshine_experiment.py
@@ -1,107 +1,42 @@
-from collections import defaultdict
 from itertools import takewhile, dropwhile
 import re
 import py_pressure_logs.regex_utils as regex_utils
-
-TIME_KEY = "time"
-PRESSURE_KEY = "pressure"
-OUTPUT_KEY = "output"
+from py_pressure_logs.experiment import Experiment
 
 
-def is_time_field(header):
-    pattern = r"time"
-    return regex_utils.regex_match(pattern, header, ignore_case=True)
+class SunshineExperiment(Experiment):
 
+    def is_pressure_field(self, header):
+        pattern = r".*(\d+).*current pressure.*"
+        return regex_utils.regex_match(pattern, header, ignore_case=True)
 
-def is_pressure_field(header):
-    pattern = r".*(\d+).*current pressure.*"
-    return regex_utils.regex_match(pattern, header, ignore_case=True)
+    def select_experiment_keys(self, row):
+        """Selects keys from raw csv data into a just the key-value map relevant for the experiment.
+        Common keys such as time and output are handled internally."""
+        formatted_row = {self.PRESSURE_KEY: {}}
+        for key, val in row.items():
+            if self.is_pressure_field(key):
+                formatted_row[self.PRESSURE_KEY][key] = val
+                continue
 
+        return formatted_row
 
-def is_output_field(header):
-    pattern = r"output"
-    return regex_utils.regex_match(pattern, header, ignore_case=True)
+    def get_experiment_number_from_start_row(self, row):
+        row_output = row[self.OUTPUT_KEY]
+        pattern = r".*Experiment (\d+):.*Experiment Preparation Begun"
+        match = re.search(pattern, row_output, re.IGNORECASE)
 
+        if match:
+            return match.group(1)
+        else:
+            raise Exception(f"no experiment number found for row {row}")
 
-def transform_row(row):
-    """Transforms a row from all csv data to a collection of log lines"""
-    formatted_row = {PRESSURE_KEY: {}}
-    for key, val in row.items():
-        if is_time_field(key):
-            formatted_row[TIME_KEY] = val
-            continue
-        elif is_output_field(key):
-            formatted_row[OUTPUT_KEY] = val
-            continue
-        elif is_pressure_field(key):
-            formatted_row[PRESSURE_KEY][key] = val
-            continue
+    def is_start_of_experiment(self, row):
+        row_output = row[self.OUTPUT_KEY]
+        pattern = r".*Experiment \d+:.*Experiment Preparation Begun"
+        return regex_utils.regex_match(pattern, row_output, ignore_case=True)
 
-    return formatted_row
-
-
-def is_start_of_experiment(row):
-    row_output = row[OUTPUT_KEY]
-    pattern = r".*Experiment \d+:.*Experiment Preparation Begun"
-    return regex_utils.regex_match(pattern, row_output, ignore_case=True)
-
-
-def is_end_of_experiment(row):
-    row_output = row[OUTPUT_KEY]
-    pattern = r"Experiment run completed"
-    return regex_utils.regex_match(pattern, row_output, ignore_case=True)
-
-
-def get_experiment_number_from_start_row(row):
-    row_output = row[OUTPUT_KEY]
-    pattern = r".*Experiment (\d+):.*Experiment Preparation Begun"
-    match = re.search(pattern, row_output, re.IGNORECASE)
-
-    if match:
-        return match.group(1)
-    else:
-        raise Exception(f"no experiment number found for row {row}")
-
-
-def extract_experiment_logs(rows):
-    """Returns all rows for an experiment, dropping all rows up to the start of the experiment"""
-    logs_from_experiment_start = dropwhile(
-        lambda row: not is_start_of_experiment(row), rows
-    )
-
-    experiment_rows = takewhile(
-        lambda row: not is_end_of_experiment(row), logs_from_experiment_start
-    )
-
-    return list(experiment_rows)
-
-
-def group_into_experiments(rows):
-    row_iterator = iter(rows)
-    grouped_results = {}
-    try:
-        while True:
-            experiment_rows = extract_experiment_logs(row_iterator)
-            if not experiment_rows or len(experiment_rows) == 0:
-                break
-
-            experiment_number = get_experiment_number_from_start_row(experiment_rows[0])
-            grouped_results[experiment_number] = experiment_rows
-
-    except StopIteration:
-        print("The experimental logs have been exhausted")
-
-    return grouped_results
-
-
-def summarise_experiment(experiment_logs):
-    """Given the logs for an experiment, calculates the maximum pressure values for each pump"""
-    grouped_values = defaultdict(list)
-
-    for log_line in experiment_logs:
-        for pump_key, pressure_value in log_line[PRESSURE_KEY].items():
-            grouped_values[pump_key].append(float(pressure_value))
-
-    max_values = {key: max(values) for key, values in grouped_values.items()}
-
-    return max_values
+    def is_end_of_experiment(self, row):
+        row_output = row[self.OUTPUT_KEY]
+        pattern = r"Experiment run completed"
+        return regex_utils.regex_match(pattern, row_output, ignore_case=True)
